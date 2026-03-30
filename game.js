d5c59d47466757ce9ddad11889585811192066de
Spyd3r05 <agnesnzivu05@gmail.com>
2026-03-30 16:21:39 +0300

    feat: enable world bounds collision for the warrior and monster

--- game.js
+++ game.js
@@ -1,19 +1,17 @@
 class mainScene {
   preload() {
-    // This method is called once at the beginning
-    // It will load all the assets, like sprites and sounds
     this.load.image("warrior", "assets/warrior.png");
     this.load.image("monster", "assets/monster.png");
   }
+
   create() {
-    // This method is called once, just after preload()
-    // It will initialize our scene, like the positions of the sprites
     this.warrior = this.physics.add.sprite(100, 100, "warrior");
     this.warrior.setScale(0.5);
+    this.warrior.setCollideWorldBounds(true);
+
     this.monster = this.physics.add.sprite(300, 300, "monster");
     this.monster.setScale(0.2);
 
-    //add score
     this.score = 0;
     this.scoreText = this.add.text(16, 16, "Score: " + this.score, {
       fontSize: "24px",
@@ -22,18 +20,25 @@ class mainScene {
 
     this.arrow = this.input.keyboard.createCursorKeys();
   }
+
   hit() {
-    //change the position of monster randomly
-    this.monster.x = Phaser.Math.Between(100, 600);
-    this.monster.y = Phaser.Math.Between(100, 600);
+    // Get the monster's half‑width and half‑height (based on its scaled size)
+    const halfWidth = this.monster.displayWidth / 2;
+    const halfHeight = this.monster.displayHeight / 2;
 
-    //increment score
+    // Define the safe range: from half‑size to (world width - half‑size)
+    const minX = halfWidth;
+    const maxX = this.physics.world.bounds.width - halfWidth;
+    const minY = halfHeight;
+    const maxY = this.physics.world.bounds.height - halfHeight;
+
+    // Place the monster randomly but fully inside the world
+    this.monster.x = Phaser.Math.Between(minX, maxX);
+    this.monster.y = Phaser.Math.Between(minY, maxY);
+
     this.score += 10;
-
-    //display updated score
     this.scoreText.setText("Score: " + this.score);
 
-    //make the warrior temporarily grow after a kill
     this.tweens.add({
       targets: this.warrior,
       scaleX: 0.6,
@@ -43,15 +48,12 @@ class mainScene {
       repeat: 1,
     });
   }
+
   update() {
-    // This method is called 60 times per second after create()
-    // It will handle all the game's logic, like movements
-    //check for hits
     if (this.physics.overlap(this.monster, this.warrior)) {
       this.hit();
     }
 
-    //handle horizontal movement
     if (this.arrow.right.isDown) {
       this.warrior.x += 2;
     }
@@ -68,10 +70,10 @@ class mainScene {
 }
 
 new Phaser.Game({
-  width: 700, // Width of the game in pixels
-  height: 400, // Height of the game in pixels
-  backgroundColor: "#225103ff", // The background color
-  scene: mainScene, // The name of the scene we created
-  physics: { default: "arcade" }, // The physics engine to use
-  parent: "game", // Create the game inside the <div id="game">
+  width: 700,
+  height: 400,
+  backgroundColor: "#225103ff",
+  scene: mainScene,
+  physics: { default: "arcade" },
+  parent: "game",
 });
